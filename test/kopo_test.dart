027f6dbd5190bd2fc5c5e840556d11abd08d23c2
Paul Han <proghjy@gmail.com>
2019-11-20 14:16:44 +0800

    Update LICENSE file

--- test/kopo_test.dart
+++ test/kopo_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kopo/kopo.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   Widget createWidgetForTesting({Widget child}) {
